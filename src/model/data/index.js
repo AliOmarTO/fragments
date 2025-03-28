4aff4978871786835b4ed34fc0040bee740116bd
Ali Omar <aomar46@myseneca.ca>
2025-03-28 18:34:33 -0400

    added s3 functionality

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,2 +1,6 @@
 // using in-memory db for now
 module.exports = require('./memory');
+
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
